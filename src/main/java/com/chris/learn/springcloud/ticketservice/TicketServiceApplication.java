a4067941c66fb0ec16c4c8b46bbad3918e6f645d
christin_chacko_qb <christin@qburst.com>
2023-12-05 00:07:30 +0530

    add eureka client properties and dependenices

--- src/main/java/com/chris/learn/springcloud/ticketservice/TicketServiceApplication.java
+++ src/main/java/com/chris/learn/springcloud/ticketservice/TicketServiceApplication.java
@@ -2,8 +2,10 @@ package com.chris.learn.springcloud.ticketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TicketServiceApplication {
 
 	public static void main(String[] args) {
